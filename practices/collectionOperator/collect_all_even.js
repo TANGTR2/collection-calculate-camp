0ff969c0944615075a61e88b3cd7176aac422690
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-07 18:07:14 +0800

    temp storage

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  let collectionEven = [];
+  for(let i in collection){
+    if( i%2 === 0 ){
+      if(i!=0)
+        collectionEven.push(i);
+    }
+  }
+  console.log(collectionEven);
+  return collectionEven;
 }
 
 module.exports = collect_all_even;
